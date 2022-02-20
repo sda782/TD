37c9b6a67ef44c9f3cc7920622f47f821147d447
sda782 <saerdna.petersen@gmail.com>
2022-02-20 14:27:40 +0100

    Turret atk indecation

--- Enemy Collapse/Assets/Scripts/EnemyManager.cs
+++ Enemy Collapse/Assets/Scripts/EnemyManager.cs
@@ -7,11 +7,8 @@ public class EnemyManager : MonoBehaviour
     private GameObject[] enemies;
     private int amount;
     private int numberofenemies = 10;
-    public static EnemySO GetRandomEnemyType { get => enemySOs[Random.Range(0, enemySOs.Length)]; }
-    private static EnemySO[] enemySOs;
     public void SpawnEnemies()
     {
-        if (enemySOs == null) enemySOs = (EnemySO[])Resources.LoadAll<EnemySO>("Enemies");
         amount = 0;
         InvokeRepeating("spawnEnemy", 0.1f, 0.3f);
     }
@@ -20,7 +17,6 @@ public class EnemyManager : MonoBehaviour
     {
         if (amount == numberofenemies - 1) CancelInvoke();
         amount++;
-        GameObject e = Instantiate(enemies[0]);
-        //e.GetComponent<EnemyData>().SetType(GetRandomEnemyType);
+        GameObject e = Instantiate(enemies[Random.Range(0, enemies.Length)]);
     }
 }
--- Enemy Collapse/Assets/Scripts/Turret.cs
+++ Enemy Collapse/Assets/Scripts/Turret.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private int lives = 10;
+    private ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
     private void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Enemy")
         {
+            ps.Play();
             EnemyData ed = col.gameObject.GetComponent<EnemyData>();
             lives -= ed.Attak();
             ed.TakeDamage(5);
